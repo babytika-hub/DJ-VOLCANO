9644a00ab975a7c60c89e758dfc59fda9f7f7ba8
babytika-hub <babytika972@hotmail.fr>
2022-04-08 15:56:20 +0200

    firt commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector(".nav_bar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+const button = document.querySelector("[data-btn]")
+button.addEventListener("click", () => {
+  button.classList.add("animating")
+})
